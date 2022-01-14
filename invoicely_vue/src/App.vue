ab46f2a2d0e5f3b3303077a8a7381fe64d6a0a33
NEOE_LEB <popla.osca@gmail.com>
2022-01-14 09:53:03 +0700

    vuejs

---
+++ invoicely_vue/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="wrapper">
+    <h1>{{ username }}</h1>
+    <p>{{ lastname }}</p>
+    <a v-bind:href="links">Link</a>
+    <h3>{{ getname() }}</h3>
+    <!-- <p> Using mustaches : {{tac}} </p>
+	<p>Using v-html directive : <span v-html="tac"></span></p>	 -->
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      username: "hello world",
+      lastname: "LASTNAME",
+      links: "https://www.google.com",
+      // tac: "<span>TAG HTML</span>"
+    };
+  },
+  methods: {
+    getname() {
+      return "HERO";
+    },
+  },
+};
+</script>
+<style lang="scss">
+@import "../node_modules/bulma";
+</style>
